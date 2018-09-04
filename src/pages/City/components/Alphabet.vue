d97e6adfc910c60d6b38ab9b4197b4b32e8e35d0
ZhihaoJian <jianzhihao123@gmail.com>
2018-09-04 21:13:28 +0800

    使用betterscroll完成滚动布局和字母表布局

---
+++ src/pages/City/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="list">
+        <ul>
+            <li class="item" >A</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "CityAlphabet"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../../../assets/css/variable.styl';
+
+.list {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    top: 1.58rem;
+    right: 0;
+    bottom: 0;
+    width: 0.4rem;
+
+    .item {
+        text-align: center;
+        line-height: 0.4rem;
+        color: $bgColor;
+    }
+}
+</style>
+
