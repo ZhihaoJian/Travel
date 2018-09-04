cd5eb632199d0d3cd2dd2fc4f71d73c49c93691b
ZhihaoJian <jianzhihao123@gmail.com>
2018-09-04 15:25:54 +0800

    新增首页swiper组件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './routes/Routes'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import './assets/css/reset.css'
 import './assets/css/border.css'
 import './assets/css/iconfont.css'
@@ -9,6 +11,7 @@ import 'fastclick'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAwesomeSwiper)
 Vue.use(VueRouter)
 
 new Vue({
