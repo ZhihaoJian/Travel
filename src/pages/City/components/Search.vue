15adc94a84f617f5089dfa1ab5b53b01dfcf9f1e
ZhihaoJian <jianzhihao123@gmail.com>
2018-09-04 20:17:11 +0800

    新增城市选择搜索框布局

---
+++ src/pages/City/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="search">
+        <input type="text" placeholder="输入城市名或拼音" class="search-input" >
+    </div>
+</template>
+
+<script>
+export default {
+  name: "CitySearch"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../../../assets/css/variable.styl';
+
+.search {
+    height: 0.76rem;
+    background: $bgColor;
+    padding: 0 0.1rem;
+
+    .search-input {
+        height: 0.62rem;
+        line-height: 0.62rem;
+        width: 100%;
+        text-align: center;
+        border-radius: 0.06rem;
+        color: #666;
+        padding: 0 0.1rem;
+        box-sizing: border-box;
+    }
+}
+</style>
