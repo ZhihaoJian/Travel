d1add6310884d3de6a7843cb8498e05efdba1dd5
ZhihaoJian <jianzhihao123@gmail.com>
2018-09-05 22:25:58 +0800

    详情页面通用渐隐头部

---
+++ src/pages/Detail/components/Header.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <router-link
+             to="/" 
+             tag="div" 
+             class='header-abs'
+             v-show="showAbs"
+        >
+            <span class='iconfont header-abs-back'>&#xe624;</span>
+        </router-link>
+        <div class='header-fixed' :style="opacityStyle" v-show="!showAbs">
+            景点详情
+            <router-link to="/">
+                <div class='iconfont header-fixed-back'>&#xe624;</div>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Gallary from '../../../common/Gallary/Gallary';
+
+    export default {
+        name:'DetailHeader',
+        data(){
+            return{
+                showAbs:true,
+                opacityStyle:{
+                    opacity:0
+                }
+            }
+        },
+        methods:{
+            handleScroll(){
+                const top = document.documentElement.scrollTop;
+                if(top > 60){
+                    let opacity = top / 140;
+                    opacity = opacity > 1 ? 1: opacity;
+                    this.opacityStyle = { opacity }
+                    this.showAbs = false;
+                }else{
+                    this.showAbs = true;
+                }
+            }
+        },
+        activated() {
+            window.addEventListener('scroll',this.handleScroll)
+        },
+    }
+</script>
+
+<style lang="stylus"  scoped>
+
+    @import '../../../assets/css/variable.styl';
+
+   .header-abs{
+       position:absolute;
+       left:.2rem;
+       top:.2rem;
+       width:.8rem;
+       text-align:center;
+       line-height:.8rem;
+       height:.8rem;
+       border-radius:.4rem;
+       background:rgba(0,0,0,.8);
+
+       .header-abs-back{
+           color:#fff;
+           font-size:.4rem;
+       }
+   }
+
+   .header-fixed {
+        position: fixed;
+        top:0;
+        left:0;
+        right:0;
+        height: $headerHeight;
+        line-height: $headerHeight;
+        text-align: center;
+        color: #fff;
+        background-color: $bgColor;
+        font-size: 0.32rem;
+
+        .header-fixed-back {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 0.64rem;
+            text-align: center;
+            font-size: 0.4rem;
+            color: #fff;
+        }
+    }
+</style>
